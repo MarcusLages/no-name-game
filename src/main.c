c5acdc7d2317d0092aa649acdb3b0ab879d20343
MarcusLages <marcusvlages@gmail.com>
2024-07-23 14:05:18 -0700

    Initialized basic black window

--- src/main.c
+++ src/main.c
@@ -1,6 +1,12 @@
 #include "raylib.h"
 #include "raymath.h"
 
+// * DEFINITIONS
+// Window rendering variables
+#define SCREEN_WIDTH    1600
+#define SCREEN_HEIGHT   900
+#define FRAME_RATE      60
+
 // * FUNCTION PROTOTYPES
 // Game life cicle functions
 static void GameStartup();  
@@ -21,8 +27,20 @@ int main() {
     return 0;
 }
 
-static void GameStartup() {}
+static void GameStartup() {
+    // Initialize window
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "No name game name");
+    SetTargetFPS(FRAME_RATE);
+}
 
 static void GameUpdate() {}
-static void GameRender() {}
-static void GameClosing() {}
+
+static void GameRender() {
+    BeginDrawing();
+        ClearBackground(BLACK);
+    EndDrawing();
+}
+
+static void GameClosing() {
+    CloseWindow();
+}
