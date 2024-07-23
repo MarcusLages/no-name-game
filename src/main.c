749ec50f79191385ae5a0e0b80bdbf0922b74ae3
MarcusLages <marcusvlages@gmail.com>
2024-07-23 14:02:09 -0700

    Setup game life cicle functions

--- src/main.c
+++ src/main.c
@@ -1,6 +1,28 @@
 #include "raylib.h"
 #include "raymath.h"
 
+// * FUNCTION PROTOTYPES
+// Game life cicle functions
+static void GameStartup();  
+static void GameUpdate();
+static void GameRender();
+static void GameClosing();
+
 int main() {
+    GameStartup();
+
+    while(!WindowShouldClose()) {
+        GameUpdate();
+        GameRender();
+    }
+
+    GameClosing();
+
     return 0;
 }
+
+static void GameStartup() {}
+
+static void GameUpdate() {}
+static void GameRender() {}
+static void GameClosing() {}
