a9992d77355f9d0de058c1abc1c178c39d08ebc6
MarcusLages <marcusvlages@gmail.com>
2024-07-24 15:26:53 -0700

    Implemented initial display of main menu

--- src/main_menu.c
+++ src/main_menu.c
@@ -2,30 +2,40 @@
 #include "raymath.h"
 #include "screen.h"
 
-//* ENUMERATIONS
+#define MAX_MENU_BUTTONS 3
 
-// Button options for the main menu
-typedef enum MenuButtons {
-    START_BUTTON = 0,
-    OPTIONS_BUTTON,
-    EXIT_BUTTON
-} MenuButtons;
+//* FUNCTIONS PROTOTYPES
 
-static int CenterComponentX(int componentWidth) {
-    return (SCREEN_WIDTH - componentWidth) / 2;
-}
+/*
+    Used to return the x position that a component should have to be
+    centered on the screen based on its width.
+    @param componentWidth
+    @return Centered x position the component should have 
+ */
+static int CenterComponentX(int componentWidth);
+
+//* VARIABLES
+
+// Array with all button text options for the main menu.
+char menuButtonsText[MAX_MENU_BUTTONS][8] = {"Start", "Options", "Exit"};
+
+// Initial generic button
+Rectangle button;
 
 void MainMenuStartup() {
     // Sets up currentScreen
     currentScreen = MAIN_MENU;
 }
 
-void MainMenuUpdate() {}
+void MainMenuUpdate() {
+    Vector2 mouse = GetMousePosition();
+
+    // TODO: make mouse collision with main menu buttons.
+}
 
 void MainMenuRender() {
-    // Initial generic button
-    Rectangle button = {
-        .x = CenterComponentX(240),
+    button = (Rectangle) {
+        .x = (float) CenterComponentX(240),
         .y = SCREEN_HEIGHT / 2,
         .width = 240,
         .height = 60
@@ -37,17 +47,16 @@ void MainMenuRender() {
     // Game name
     DrawText("No Name Game Name", CenterComponentX(460), SCREEN_HEIGHT / 3, 50, BLACK);
 
-    // Start button
-    DrawRectangle(button.x, button.y, button.width, button.height, BLACK);
-    DrawText("Start", CenterComponentX(MeasureText("Start", 40)), button.y + 10, 40, RAYWHITE);
+    for(int i = 0; i < MAX_MENU_BUTTONS; i++) {
+        DrawRectangle(button.x, button.y, button.width, button.height, BLACK);
+        DrawText(menuButtonsText[i], CenterComponentX(MeasureText(menuButtonsText[i], 40)), button.y + 10, 40, RAYWHITE);
+        button.y += button.height + 20;
+    }
 
-    // Options button
-    button.y += button.height + 20;
-    DrawRectangle(button.x, button.y, button.width, button.height, BLACK);
-
-    // Quit button
-    button.y += button.height + 20;
-    DrawRectangle(button.x, button.y, button.width, button.height, BLACK);
 }
 
 void MainMenuUnload() {}
+
+static int CenterComponentX(int componentWidth) {
+    return (SCREEN_WIDTH - componentWidth) / 2;
+}
