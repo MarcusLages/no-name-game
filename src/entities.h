ffe918ec8d1edae4f783a49fd027bea35007ab9c
MarcusLages <marcusvlages@gmail.com>
2024-07-24 15:57:46 -0700

    Created player.c

--- src/entities.h
+++ src/entities.h
@@ -1,9 +1,7 @@
 #ifndef ENTITIES_H
 #define ENTITIES_H
 
-#include <stdbool.h>
-
-// State of entities for animation and properties
+// Enum for the action state of entities for animation and properties.
 typedef enum GameState {
     IDLE = 0,
     MOVING,
@@ -11,17 +9,24 @@ typedef enum GameState {
 } GameState;
 
 //* Player
+// Structure to represent the player.
 typedef struct Player {
     int x;
     int y;
     GameState state;
 } Player;
 
+//* Enemy
+// Structure to represent the player.
 typedef struct Enemy {
     int x;
     int y;
     GameState state;
     int health;
 } Enemy;
+
+//* VARIABLES
+
+extern Player player;
 
 #endif // !ENTITIES_H
---
+++ src/player.c
@@ -0,0 +1,21 @@
+#include "raylib.h"
+#include "raymath.h"
+#include "entities.h"
+
+Player player;
+
+void PlayerStartup() {
+    
+}
+
+void PlayerUpdate() {
+
+}
+
+void PlayerRender() {
+
+}
+
+void PlayerUnload() {
+
+}
