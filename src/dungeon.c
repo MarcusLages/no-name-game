88da78e179d82ea26b46b88329711ee2a24121b9
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-23 17:50:14 -0700

    added comments

--- src/dungeon.c
+++ src/dungeon.c
@@ -1,19 +1,27 @@
 #include "screen.h"
 #include "texture.h"
 
+// 2D array of type Tile for the world level
 Tile **world;
+
+// A reference to the game's tilemap
 Texture2D *textures;
 
 void DungeonStartup() {
+    // Sets the current screen 
     currentScreen = DUNGEON;
 
+    // Loads the game's tilemap into heap as Texture2D
     Image img = LoadImage("resources/tilemap.png");
     textures = (Texture2D*) malloc(sizeof(Texture2D));
     textures[1] = LoadTextureFromImage(img);
+
+    // Unloads the image as it is no longer needed
     UnloadImage(img);
 
     world = (Tile**) malloc(WORLD_WIDTH * sizeof(Tile*));
 
+    // Allocating tiles of type Tile into 2D array
     for (int j = 0; j < WORLD_HEIGHT; j++) {
         Tile *tiles = (Tile*) malloc(WORLD_HEIGHT * sizeof(Tile));
         for (int i = 0; i < WORLD_WIDTH; i++) {
@@ -32,6 +40,7 @@ void DungeonUpdate() {}
 void DungeonRender() {
     Tile tile;
 
+    // Rendering each tile by calling DrawTile
     for (int j = 0; j < WORLD_HEIGHT; j++) {
         for (int i = 0; i < WORLD_WIDTH; i++) {
             tile = world[i][j];
@@ -49,6 +58,14 @@ void DungeonUnload() {
     world = NULL;
 }
 
+/**
+ * This function is reponsible for drawing tiles at (x, y) from the tilemap at (x, y).
+ * 
+ * @param x_pos the x-coord to draw on
+ * @param y_pos the y-coord to draw on
+ * @param texture_tile_x the x-coord the tile in present on
+ * @param texture_tile_y the y-coord the tile in present on
+ */
 void DrawTile(int x_pos, int y_pos, int texture_tile_x, int texture_tile_y) {
     Rectangle source = { 
         (float) (texture_tile_x * TILE_WIDTH), 
