7b5599358681d175e7a997424bf8167efbf8b70d
samarjit bhogal <samarjit.v.bhogal@gmail.com>
2024-07-24 22:34:47 -0700

    added additional comments

--- src/sprites.c
+++ src/sprites.c
@@ -8,11 +8,14 @@ Animation idlePlayerAnimation;
 
 void SpriteUpdate() {
     int numOfTiles = FindNumOfTiles(ENTITY_TILE_WIDTH, TILE_PLAYER_IDLE);
+
+    // Initializing the indle animation
     idlePlayerAnimation = CreateAnimation(ENTITY_IDLE_FPS, 
         numOfTiles,
         GetSpriteRectangles(numOfTiles, ENTITY_TILE_WIDTH, ENTITY_TILE_HEIGHT, TILE_PLAYER_IDLE), 
         textures[TILE_PLAYER_IDLE]);
 
+    //
     DrawAnimation(idlePlayerAnimation, (Rectangle) {0, 0, ENTITY_TILE_WIDTH, ENTITY_TILE_HEIGHT}, 0.0f, WHITE);
 }
 
@@ -37,14 +40,19 @@ void UnloadAnimation(Animation animation) {
 }    
 
 void DrawAnimation(Animation animation, Rectangle dest, float rotation, Color color) {
+    // grabing a new index every new frame since the game was started
     int idx = (int) (GetTime() * animation.fps) % animation.numOfRectangles;
     Rectangle source = animation.rectangles[idx];
     Vector2 origin = { 0, 0 };
+
     DrawTexturePro(animation.textures, source, dest, origin, rotation, color);
 }
 
 Rectangle* GetSpriteRectangles(int numOfRectangles, int tileWidth, int tileHeight, TextureFile textureFile) {
+    // Allocating memory for the appropiate amount of rectangles
     Rectangle *rectangles = (Rectangle*) malloc(sizeof(Rectangle) * numOfRectangles);
+
+    // Populating the array
     for (int i = 0; i < numOfRectangles; i++) {
         Rectangle r = {i * tileWidth, 0, tileWidth, tileHeight};
         rectangles[i] = r;
