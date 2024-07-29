b1ea0ebd86becf131e481178810cb5a8f6d9034c
MarcusLages <marcusvlages@gmail.com>
2024-07-29 15:47:07 -0700

    Better documentation for RayRectCollision() on collision.c

--- src/collision.c
+++ src/collision.c
@@ -35,55 +35,60 @@ RayCollision2D RayRectCollision(Ray2D ray, Rectangle hitbox) {
     RayCollision2D collision;
     collision.hit = false;
 
-    // Instead of using absolute direction from the origin point
-    // we are changing it to use the direction based on the origin point
+    // ! WARNING:
+    // ! Before doing all calculations, if the direction vector is absolute,
+    // ! it's important to make it relative to the origin vector.
     // ray.direction.x -= ray.origin.x;
     // ray.direction.y -= ray.origin.y;
     
     // Get the near collision point and far collision point in terms
     // of time of collision normalized from 0 to 1.
     // 0 represents the origin and 1 represents the destination
-    Vector2 nearCol = (Vector2) {
+
+    // Near collision
+    Vector2 nearColTime = (Vector2) {
         .x = (hitbox.x - ray.origin.x) / ray.direction.x,
         .y = (hitbox.y - ray.origin.y) / ray.direction.y
     };
+
     // Far collision
-    Vector2 farCol = (Vector2) {
+    Vector2 farColTime = (Vector2) {
         .x = (hitbox.x + hitbox.width - ray.origin.x) / ray.direction.x,
         .y = (hitbox.y + hitbox.height - ray.origin.y) / ray.direction.y
     };
     
     // Sort the collision point in case they are in different quadrants
     // instead of the origin quadrant (0,0)
-    if (nearCol.x > farCol.x) swap(&nearCol.x, &farCol.x);
-    if (nearCol.y > farCol.y) swap(&nearCol.y, &farCol.y);
+    if (nearColTime.x > farColTime.x) swap(&nearColTime.x, &farColTime.x);
+    if (nearColTime.y > farColTime.y) swap(&nearColTime.y, &farColTime.y);
 
     // Checks for collision rules, if they are not met,
     // returns RayCollision2D structure with no hit
-    if (nearCol.x > farCol.y || nearCol.y > farCol.x)
+    if (nearColTime.x > farColTime.y || nearColTime.y > farColTime.x)
         return collision;
 
     // Calculate time of collision
-    collision.timeHit = max(nearCol.x, nearCol.y);
-    float farTime = max(farCol.x, farCol.y);
+    collision.timeHit = max(nearColTime.x, nearColTime.y);
+    float farTime = max(farColTime.x, farColTime.y);
 
-    // Rejects collisions that happen before the origin point of the ray
+    // Rejects collisions that happen entirely before the origin point of the ray (at negative time).
+    // ? NOTE: Depending on your use of ray collision, you might need to fine tune this.
     if (farTime < 0) return collision;
 
-    // Rejects collisions that happen after the destination point of the ray
-    // which means the ray is on the direction of the rectangle, but not there yet
-    if (collision.timeHit >= 1) return collision;
+    // Used to reject collisions that happen after the destination point of the ray
+    // which means the ray is on the direction of the rectangle, but not there yet.
+    // Commented because it should be used outside when treating the collision.
+
+    // if (collision.timeHit > 1) collision.hit = false;
+    // else collision.hit = true;
 
     // Retrieving information on the contact point
-    collision.contactPoint = (Vector2) {
-        .x = ray.origin.x + collision.timeHit * ray.direction.x,
-        .y = ray.origin.y + collision.timeHit * ray.direction.y,
-    };
+    collision.contactPoint = Vector2Add(ray.origin, Vector2Scale(ray.direction, collision.timeHit));
 
     // Retrieving information on the normal vector of the contact point,
     // or in an easier way to understand,
     // vector with the opposite in which direction the ray touches the rectangle
-    if (nearCol.x > nearCol.y) {
+    if (nearColTime.x > nearColTime.y) {
         if (ray.direction.x < 0)
             collision.normalVector = (Vector2) { 1, 0 };
         else
